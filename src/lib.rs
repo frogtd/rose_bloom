98e5ac8a815db05e9d040cac7cd43aa7c19f74b7
frogtd <ash@frogtd.com>
2022-08-24 13:45:39 -0400

    change api to use &self instead on push

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,26 @@
 #![forbid(unsafe_op_in_unsafe_fn)]
-use std::{
-    alloc::{self, Layout},
-    marker::PhantomData,
-    mem,
-    ptr::{self, addr_of_mut, NonNull},
+#![no_std]
+extern crate alloc;
+use core::{
+    alloc::Layout,
+    mem::{self, ManuallyDrop},
+    ops::Index,
+    ptr::{self, addr_of, addr_of_mut, NonNull},
+    sync::atomic::{AtomicPtr, AtomicUsize, Ordering},
 };
-pub struct Rose<T> {
-    last: NonNull<RoseInner<T>>,
-}
 
-unsafe impl<T> Send for Rose<T> where T: Send {}
-unsafe impl<T> Sync for Rose<T> where T: Sync {}
+use alloc::alloc::{alloc, dealloc, handle_alloc_error};
+pub struct Rose<T> {
+    last: AtomicPtr<RoseInner<T>>,
+}
 
 #[repr(C)]
 struct RoseInner<T> {
     capacity: usize,
-    length: usize,
+    length: AtomicUsize,
     next: *mut RoseInner<T>,
     data: [T; 0],
 }
-impl<T> Clone for RoseInner<T> {
-    fn clone(&self) -> Self {
-        Self {
-            capacity: self.capacity,
-            length: self.length,
-            next: self.next,
-            data: [],
-        }
-    }
-}
-impl<T> std::fmt::Debug for RoseInner<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("RoseInner")
-            .field("capacity", &self.capacity)
-            .field("length", &self.length)
-            .field("next", &self.next)
-            .finish()
-    }
-}
-#[derive(Debug)]
-pub struct Petal<'a, T>(*const T, PhantomData<&'a T>);
-
-// Note: do these require a T: Send/Sync bound?
-unsafe impl<'a, T> Send for Petal<'a, T> {}
-unsafe impl<'a, T> Sync for Petal<'a, T> {}
 
 const fn next_multiple_of(n: usize, m: usize) -> usize {
     match n % m {
@@ -84,76 +61,129 @@ impl<T> Rose<T> {
         assert!(Self::size(1) != 0);
         unsafe {
             let layout = Self::layout(capacity);
-            let memory = alloc::alloc(layout);
+            let memory = alloc(layout);
             if memory.is_null() {
-                alloc::handle_alloc_error(layout)
+                handle_alloc_error(layout)
             }
             NonNull::new_unchecked(memory as *mut RoseInner<T>)
         }
     }
     pub fn new() -> Rose<T> {
         // Allocation code
-        let memory = Self::alloc(1);
+        let memory = Self::alloc(1).as_ptr();
         // Initialization code
         unsafe {
-            let memory = memory.as_ptr();
             addr_of_mut!((*memory).capacity).write(1);
-            addr_of_mut!((*memory).length).write(0);
+            addr_of_mut!((*memory).length).write(AtomicUsize::new(0));
             addr_of_mut!((*memory).next).write(ptr::null_mut());
         };
-        Rose { last: memory }
+        Rose {
+            last: AtomicPtr::new(memory),
+        }
     }
 
-    pub fn push(&mut self, value: T) -> &T {
-        // SAFETY: Last is always a valid pointer.
-        let capacity = unsafe { (*self.last.as_ptr()).capacity };
-        let length = unsafe { (*self.last.as_ptr()).length };
-        if capacity != length {
-            // We have space.
-            unsafe {
-                let out = ptr::addr_of_mut!((*self.last.as_ptr()).data)
-                    .cast::<T>()
-                    .add((*self.last.as_ptr()).length);
-                out.write(value);
-                (*self.last.as_ptr()).length += 1;
-                &*out
-            }
-        } else {
-            // We need to alloc more.
-            let new = Self::alloc(capacity * 2);
-            let last = self.last;
-            unsafe {
-                let new = new.as_ptr();
-                ptr::addr_of_mut!((*new).capacity).write(capacity * 2);
-                ptr::addr_of_mut!((*new).length).write(0);
-                ptr::addr_of_mut!((*new).next).write(last.as_ptr());
-            }
-            // We have setup the new pointer.
-            self.last = new;
-            // Now we have space.
-            unsafe {
-                let out = ptr::addr_of_mut!((*self.last.as_ptr()).data)
-                    .cast::<T>()
-                    .add((*self.last.as_ptr()).length);
-                out.write(value);
-                (*self.last.as_ptr()).length += 1;
-                &*out
+    pub fn push(&self, value: T) -> &T {
+        let item = ManuallyDrop::new(value);
+        loop {
+            // I just need to get access to some last element, it doesn't matter if it's out of date.
+            // If it is, we will just try again.
+            let last = self.last.load(Ordering::Relaxed);
+
+            // Both of these are from the same `RoseInner`
+            // SAFETY: Last is always a valid pointer.
+            let length = unsafe { &(*last).length };
+            let capacity = unsafe { (*last).capacity };
+            // We only need access to this one piece of memory, so we use Relaxed.
+            let index = length.fetch_update(Ordering::Relaxed, Ordering::Relaxed, |x| {
+                if x == capacity {
+                    // We need to allocate more.
+                    None
+                } else {
+                    Some(x + 1)
+                }
+            });
+
+            match index {
+                Ok(len) => {
+                    // We now "own" the index, as the index was increased.
+                    unsafe {
+                        let out = ptr::addr_of_mut!((*last).data).cast::<T>().add(len);
+                        out.write(((&item) as *const ManuallyDrop<T>).cast::<T>().read());
+                        return &*out;
+                    }
+                }
+                Err(_) => {
+                    let new = Self::alloc(capacity * 2);
+
+                    let refer = unsafe {
+                        let new = new.as_ptr();
+                        ptr::addr_of_mut!((*new).capacity).write(capacity * 2);
+                        ptr::addr_of_mut!((*new).length).write(AtomicUsize::new(1));
+                        ptr::addr_of_mut!((*new).next).write(last);
+                        // We just created the pointer, so we can do whatever we want with it.
+                        let out = ptr::addr_of_mut!((*new).data).cast::<T>();
+                        out.write((&item as *const ManuallyDrop<T>).cast::<T>().read());
+                        &*out
+                    };
+                    let success =
+                        self.last
+                            .fetch_update(Ordering::AcqRel, Ordering::Acquire, |x| {
+                                if x == last {
+                                    Some(new.as_ptr())
+                                } else {
+                                    // Somebody changed our pointer.
+                                    None
+                                }
+                            });
+                    match success {
+                        Ok(_) => return refer,
+                        Err(_) => {
+                            unsafe {
+                                dealloc(new.as_ptr().cast::<u8>(), Self::layout(capacity * 2))
+                            };
+                            continue;
+                        }
+                    }
+                }
             }
         }
     }
 }
 
+/// This index is the number from the back.
+impl<T> Index<usize> for Rose<T> {
+    type Output = T;
+
+    fn index(&self, mut index: usize) -> &Self::Output {
+        let mut current = self.last.load(Ordering::Relaxed);
+        while !current.is_null() {
+            let length = unsafe { (*current).length.load(Ordering::Relaxed) };
+            if length > index {
+                // Great, we found it
+                // SAFETY: add is in bounds because length > index 
+                return unsafe {
+                    &*(addr_of!((*current).data)
+                        .cast::<T>()
+                        .add(length - index - 1))
+                };
+            }
+            // If we didn't find it, go to the next list item.
+            index -= length;
+            current = unsafe { (*current).next };
+        }
+        panic!("Index {index} doesn't exist.")
+    }
+}
 impl<T> Drop for Rose<T> {
     fn drop(&mut self) {
-        let mut current = self.last.as_ptr();
+        let mut current = self.last.load(Ordering::Relaxed);
         while !current.is_null() {
-            println!("DATA: \"{:?}\"", unsafe { (*current).clone() });
-
             // SAFETY: current is valid, data will have valid data for `length` items
+            let length = unsafe { (*current).length.load(Ordering::Relaxed) };
             let slice = unsafe {
-                std::slice::from_raw_parts_mut(
+                core::slice::from_raw_parts_mut(
                     ptr::addr_of_mut!((*current).data).cast::<T>(),
-                    (*current).length,
+                    length,
                 )
             };
             // Drop items
@@ -162,7 +192,7 @@ impl<T> Drop for Rose<T> {
             let next = unsafe { (*current).next };
             // Dealloc Rose
             // Current is a pointer allocated by the GlobalAlloc.
-            unsafe { alloc::dealloc(current.cast::<u8>(), Self::layout((*current).capacity)) }
+            unsafe { dealloc(current.cast::<u8>(), Self::layout((*current).capacity)) }
 
             current = next;
         }
@@ -175,8 +205,13 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let mut rose: Rose<Box<i32>> = Rose::new();
+        use alloc::boxed::Box;
+        let rose = Rose::new();
         rose.push(Box::new(1));
-        rose.push(Box::new(5));
+        rose.push(Box::new(2));
+        rose.push(Box::new(3));
+        assert_eq!(*rose[0], 3);
+        assert_eq!(*rose[1], 2);
+        assert_eq!(*rose[2], 1);
     }
 }
